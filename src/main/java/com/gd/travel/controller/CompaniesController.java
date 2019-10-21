afefe687dd0f1c8a1b01925edac3b593f92db2f3
gd <455607047@qq.com>
2019-10-21 21:21:57 +0800

    测试类

--- src/main/java/com/gd/travel/controller/CompaniesController.java
+++ src/main/java/com/gd/travel/controller/CompaniesController.java
@@ -1,9 +1,17 @@
 package com.gd.travel.controller;
 
 
+import com.gd.travel.common.baseEntity.ResultVO;
+import com.gd.travel.entity.Companies;
+import com.gd.travel.service.ICompaniesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/companies")
 public class CompaniesController extends BaseController {
+
+    @Autowired
+    private ICompaniesService companiesService;
+
+    @GetMapping("index")
+    public ResultVO<List<Companies>> index(){
+        return ResultVO.getSuccess(companiesService.list());
+    }
 
 }
 
