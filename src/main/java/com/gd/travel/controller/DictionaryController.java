4c358095ecf905e469aeeaa660baaeca053b068b
GZPING <455607047@qq.com>
2019-10-24 00:14:59 +0800

    修改数据库连接配置

--- src/main/java/com/gd/travel/controller/DictionaryController.java
+++ src/main/java/com/gd/travel/controller/DictionaryController.java
@@ -1,10 +1,21 @@
 package com.gd.travel.controller;
 
 
+import com.gd.travel.common.baseEntity.ResultVO;
+import com.gd.travel.entity.Dictionary;
+import com.gd.travel.service.ICompaniesService;
+import com.gd.travel.service.IDictionaryService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -15,12 +26,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2019-10-21
  */
 @RestController
-    @RequestMapping("/dictionary")
+@RequestMapping("/dictionary")
 public class DictionaryController extends BaseController {
+
+    @Autowired
+    private IDictionaryService dictionaryService;
 
     @GetMapping("index")
     public String index(){
         return "ok";
+    }
+
+    public ResultVO<List<Dictionary>> getDictionarysByPid(@NotNull @RequestParam("pid") Long pid){
+        return ResultVO.getSuccess(dictionaryService.listbyPid(pid));
     }
 
 }
--- src/main/java/com/gd/travel/service/impl/DictionaryServiceImpl.java
+++ src/main/java/com/gd/travel/service/impl/DictionaryServiceImpl.java
@@ -4,7 +4,10 @@ import com.gd.travel.entity.Dictionary;
 import com.gd.travel.mapper.DictionaryMapper;
 import com.gd.travel.service.IDictionaryService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +20,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class DictionaryServiceImpl extends ServiceImpl<DictionaryMapper, Dictionary> implements IDictionaryService {
 
+    @Autowired
+    private DictionaryMapper dictionaryMapper;
+
+    @Override
+    public List<Dictionary> listbyPid(Long pid) {
+        return dictionaryMapper.listByPid(pid);
+    }
 }
