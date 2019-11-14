6d5f6d759690602e752c345d2d78c1d249b8bac8
GZPING <455607047@qq.com>
2019-11-14 23:26:17 +0800

    test

--- src/test/java/com/gd/travel/TravelApplicationTests.java
+++ src/test/java/com/gd/travel/TravelApplicationTests.java
@@ -4,13 +4,16 @@ package com.gd.travel;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 @SpringBootTest
 class TravelApplicationTests {
 
     @Test
     void contextLoads() {
 
-        Integer.parseInt(null);
+
+
     }
 
 }
