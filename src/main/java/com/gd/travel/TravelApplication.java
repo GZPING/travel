26245f1b16993a9d110da3d4d94cc066ad3aaa05
guozhiping <zhiping.guo@pingcl.com>
2020-04-02 18:41:56 +0800

    spring 搞一下

--- src/main/java/com/gd/travel/TravelApplication.java
+++ src/main/java/com/gd/travel/TravelApplication.java
@@ -3,6 +3,8 @@ package com.gd.travel;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author Admin
@@ -12,7 +14,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TravelApplication.class, args);
+       /* SpringApplication.run(TravelApplication.class, args);*/
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationfile.xml");
+        System.out.println(context.getBean("user"));
+
     }
 
 
